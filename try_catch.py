1fe6cd530e5814534e0c97621445660eb5849108
abiranmol <71303230+abiranmol@users.noreply.github.com>
2020-11-01 15:17:55 +0530

    Update and rename README.md to try_catch.py

---
+++ try_catch.py
@@ -0,0 +1,12 @@
+# Python-learn
+
+def raises_value_error():
+  raise ValueError
+try:
+   
+  raises_value_error()
+
+except ValueError:
+  print("You raised a ValueError!")
+  
+ // output - You raised a ValueError!
